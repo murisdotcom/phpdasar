1536c3010deec8a9bf8a05a79397e6f6c6561b22
murisdotcom <muhrizalsidik@gmail.com>
2022-02-24 23:22:56 +0700

    Mengubah tampilan admin.php dengan bootstrap

--- login/admin.php
+++ login/admin.php
@@ -36,89 +36,146 @@ if (isset($_POST["cari"])){
 // $mahasiswa = mysqli_fetch_assoc($result);
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Halaman Admin</title>
-    <style>
-      .loader{
-        width:100px;
-        position:absolute;
-        top:126px;
-        z-index:-1;
-        left:370px;
-        display:none;
-      }
-    </style>
-  </head>
-  <body>
-    <a href="logout.php">Logout</a> |
-    <a href="cetak.php" target="_blank">cetak</a>
-    <h1>Daftar Mahasiswa</h1>
-    <br>
-    <a href="tambah.php">Tambah Data Mahasiswa</a>
-    <br>
-    <br>
-    
-    <form action="" method="post">
-      <input type="text" name="keyword" size="50" autofocus placeholder="masukkan keyword pencarian..." autocomplete="off" id="keyword">
-      <button type="submit" name="cari" id="tombol-cari">Cari</button>
+<!doctype html>
+<html lang="en">
 
-      <img src="../img/loader.gif" class="loader">
+<head>
+  <title>Halaman Admin</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    </form>
-<br>
-    <!-- navigasi -->
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-    <?php if( $halamanAktif > 1) :?>
-      <a href="?halaman=<?=$halamanAktif-1;?>">&laquo;</a>
-    <?php endif; ?>
+  <!-- My CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <style>
+    .loader {
+      width: 100px;
+      position: absolute;
+      top: 120px;
+      z-index: -1;
+      left: 430px;
+      display: none;
+    }
+  </style>
+</head>
 
-    <?php for($i=1; $i <= $jumlahHalaman; $i++) :?>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="col">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="admin.php">MS GLOW</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="admin.php">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php">Daftar Reseller</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cetak.php" target="_blank">Cetak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="col">
+    <div class="container-fluid">
+      <h1>Daftar Reseller</h1>
+      <br>
+      <a class="btn btn-primary" href="tambah.php">Tambah Data Reseller</a>
+      <br>
+      <br>
+
+      <form action="" method="post">
+        <input type="text" name="keyword" size="50" autofocus placeholder="masukkan keyword pencarian..."
+          autocomplete="off" id="keyword">
+        <button type="submit" name="cari" id="tombol-cari">Cari</button>
+
+        <img src="../img/loader.gif" class="loader">
+
+      </form>
+      <br>
+      <!-- navigasi -->
+
+      <?php if( $halamanAktif > 1) :?>
+      <a class="btn btn-primary" href="?halaman=<?=$halamanAktif-1;?>">&laquo;</a>
+      <?php endif; ?>
+
+      <?php for($i=1; $i <= $jumlahHalaman; $i++) :?>
       <?php if($i==$halamanAktif) :?>
       <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color:blue"><?= $i; ?></a>
       <?php else : ?>
       <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
       <?php endif; ?>
-    <?php endfor;?>
+      <?php endfor;?>
 
-    <?php if( $halamanAktif < $jumlahHalaman) :?>
-      <a href="?halaman=<?=$halamanAktif+1;?>">&raquo;</a>
-    <?php endif; ?>
-    <br>
-    <br>
-    <div id="container">
-      <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-          <th>No.</th>
-          <th>Aksi</th>
-          <th>Gambar</th>
-          <th>NPM</th>
-          <th>Nama</th>
-          <th>Email</th>
-          <th>Jurusan</th>
-        </tr>
-        <?php $i=1; ?>
-        <?php foreach( $mahasiswa as $mhs) :?>
-          <tr>
-            <td><?= $i; ?></td>
-            <td>
-              <a href="ubah.php?id=<?= $mhs["id"]; ?>">Ubah</a> |
-              <a href="hapus.php?id=<?= $mhs["id"]; ?>" onclick="return confirm('Yakin hapus ?');">Hapus</a>
-            </td>
-            <td>
-              <img src="../img/<?= $mhs["gambar"]; ?>" width="70">
-            </td>
-            <td><?= $mhs["npm"]; ?></td>
-            <td><?= $mhs["nama"]; ?></td>
-            <td><?= $mhs["email"]; ?></td>
-            <td><?= $mhs["jurusan"]; ?></td>
-          </tr>
-          <?php $i++; ?>
-        <?php endforeach; ?>
-      </table>
+      <?php if( $halamanAktif < $jumlahHalaman) :?>
+      <a class="btn btn-primary" href="?halaman=<?=$halamanAktif+1;?>">&raquo;</a>
+      <?php endif; ?>
+      <br>
+      <br>
+      <div id="container">
+
+        <table class="table table-striped" border="1" cellpadding="10" cellspacing="0">
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Gambar</th>
+              <th scope="col">NPM</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Email</th>
+              <th scope="col">Jurusan</th>
+              <th scope="col">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php $i=1; ?>
+            <?php foreach( $mahasiswa as $mhs) :?>
+            <tr>
+              <th scope="row"><?= $i; ?></th>
+              <td>
+                <img src="../img/<?= $mhs["gambar"]; ?>" width="70">
+              </td>
+              <td><?= $mhs["npm"]; ?></td>
+              <td><?= $mhs["nama"]; ?></td>
+              <td><?= $mhs["email"]; ?></td>
+              <td><?= $mhs["jurusan"]; ?></td>
+              <td>
+                <a class="btn btn-success" href="ubah.php?id=<?= $mhs["id"]; ?>">Ubah</a>
+                <a class="btn btn-danger" href="hapus.php?id=<?= $mhs["id"]; ?>"
+                  onclick="return confirm('Yakin hapus ?');">Hapus</a>
+              </td>
+            </tr>
+            <?php $i++; ?>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
   <script src="js/jquery-3.6.0.min.js"></script>
   <script src="js/script.js"></script>
-  </body>
+
+  <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+  </script> -->
+</body>
+
 </html>
--- login/ajax/mahasiswa.php
+++ login/ajax/mahasiswa.php
@@ -16,32 +16,36 @@ $query = "SELECT * FROM mahasiswa
 $mahasiswa = query($query);
 ?>
 
-<table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-          <th>No.</th>
-          <th>Aksi</th>
-          <th>Gambar</th>
-          <th>NPM</th>
-          <th>Nama</th>
-          <th>Email</th>
-          <th>Jurusan</th>
-        </tr>
-        <?php $i=1; ?>
-        <?php foreach( $mahasiswa as $mhs) :?>
-          <tr>
-            <td><?= $i; ?></td>
-            <td>
-              <a href="ubah.php?id=<?= $mhs["id"]; ?>">Ubah</a> |
-              <a href="hapus.php?id=<?= $mhs["id"]; ?>" onclick="return confirm('Yakin hapus ?');">Hapus</a>
-            </td>
-            <td>
-              <img src="../img/<?= $mhs["gambar"]; ?>" width="70">
-            </td>
-            <td><?= $mhs["npm"]; ?></td>
-            <td><?= $mhs["nama"]; ?></td>
-            <td><?= $mhs["email"]; ?></td>
-            <td><?= $mhs["jurusan"]; ?></td>
-          </tr>
-          <?php $i++; ?>
-        <?php endforeach; ?>
-      </table>
+<table class="table table-striped" border="1" cellpadding="10" cellspacing="0">
+  <thead>
+    <tr>
+      <th>No.</th>
+      <th>Aksi</th>
+      <th>Gambar</th>
+      <th>NPM</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php $i=1; ?>
+    <?php foreach( $mahasiswa as $mhs) :?>
+    <tr>
+      <td><?= $i; ?></td>
+      <td>
+        <a href="ubah.php?id=<?= $mhs["id"]; ?>">Ubah</a> |
+        <a href="hapus.php?id=<?= $mhs["id"]; ?>" onclick="return confirm('Yakin hapus ?');">Hapus</a>
+      </td>
+      <td>
+        <img src="../img/<?= $mhs["gambar"]; ?>" width="70">
+      </td>
+      <td><?= $mhs["npm"]; ?></td>
+      <td><?= $mhs["nama"]; ?></td>
+      <td><?= $mhs["email"]; ?></td>
+      <td><?= $mhs["jurusan"]; ?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+  <tbody>
+</table>

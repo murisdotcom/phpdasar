478ea4329d4085f5e756334926f76f58f5323616
murisdotcom <muhrizalsidik@gmail.com>
2022-02-24 22:00:31 +0700

    Mengubah tampilan login menggunakan bootstrap 4

--- login/login.php
+++ login/login.php
@@ -54,42 +54,79 @@ if (isset($_POST["login"])){
     $error = true;
   }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Halaman Login</title>
-    <!-- <style>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Halaman Login</title>
+  <!-- <style>
       label {
         display: block;
       }
     </style> -->
-  </head>
-  <body>
-    <h1>Login Admin</h1>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <?php if(isset($error)) :?>
-      <p style= "color: red; font-style: italic;">Username / Password Salah !</p>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="login">
+  <div class="logo">
+    <img src="../img/msglow.png" alt="">
+  </div>
+
+  <div class="content">
+    <form action="" method="post">
+      <div class="form-group">
+        <!-- <label for="username">Username :</label> -->
+        <input type="text" class="form-control" name="username" id="username" autocomplete="off"
+          aria-describedby="emailHelp" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <!-- <label for="password">Password :</label> -->
+        <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+      </div>
+      
+      <?php if(isset($error)) :?>
+    <p style="color: red; font-style: italic;"><b>Username / Password Salah !</b></p>
     <?php endif; ?>
 
-    <form action="" method="post">
-      <li>
-        <label for="username">Username :</label>
-        <input type="text" name="username" id="username" autocomplete="off">
-      </li>
-      <li>
-        <label for="password">Password :</label>
-        <input type="password" name="password" id="password">
-      </li>
-      <li>
-        <input type="checkbox" name="remember" id="remember">
-        <label for="remember">Remember me</label>
-      </li>
-      <br>
-      <li>
-      <button type="submit" name="login">Login</button>
-      </li>  
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" name="remember" id="remember">
+        <label class="form-check-label" for="remember">Remember me</label>
+      </div>
+      
+      <button type="submit" class="btn btn-primary" name="login">Login</button>
     </form>
-<br>
-    <span>Klik disini untuk <a href="registrasi.php">registrasi</a></span>
-  </body>
+    <!-- <span class="span">Klik disini untuk <a href="registrasi.php">registrasi</a></span> -->
+  </div>
+  <div class="copyright">
+  <span><i>Copyright &copy; <?= date("Y"); ?> <b>muris.com</b></i></span>
+  </div>
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+  </script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+</body>
+
 </html>
